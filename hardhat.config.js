adfd420b8d962819304f62f608a0b13c17d0be10
vladyslavkolesnyk1 <vladkolesnyk7@gmail.com>
2023-04-05 20:11:17 +0300

    ADD nft contract tests UPDATE refactor nft contract and fix a few bugs #LASO-53

--- hardhat.config.js
+++ hardhat.config.js
@@ -21,7 +21,6 @@ module.exports = {
             optimizer: {
                 enabled: true,
                 runs: 200,
-                details: { yul: false },
             },
         },
     },
